03e86f5b2d7bd5eda03259f5c3fa1218b72dbcfc
Leandro Souza <leandrobh@outlook.com>
2013-04-14 21:22:07 -0300

    commit

--- ConexaoJDBC/src/leandro/Principal.java
+++ ConexaoJDBC/src/leandro/Principal.java
@@ -1,6 +1,5 @@
 package leandro;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -17,6 +16,8 @@ public class Principal {
 	private Properties properties;
 	private Boolean executarDDL = false;
 	private Boolean executarInsert = false;
+	private Boolean executarTruncate = false;
+	private Boolean executarInsertMilhoes = true;
 	private TipoBase tipoBase = TipoBase.ORACLE;
 	
 	private static final String createTableOracle = "create table pessoa (ident number, nome varchar(30))";
@@ -34,24 +35,24 @@ public class Principal {
 //		this.ddl(conn, sqlDDL1);
 
 		if (this.executarDDL){
-<<<<<<< HEAD
 			String sqlDDL2 = tipoBase.equals(TipoBase.ORACLE) ? createTableOracle : createTableMysql;
 			this.ddl(conn, sqlDDL2);
 		}
 		
 		if (this.executarTruncate){
 			String sqlDDL = "truncate table pessoa";
-=======
-			String sqlDDL = "create table pessoa (nome varchar(40));";
->>>>>>> 02acc7b699465b556c1c9743d0cc05b11ac5a540
 			this.ddl(conn, sqlDDL);
 		}
 
 		if (this.executarInsert){
-			String sqlInsert = "insert into pessoa values (?)";
+			String sqlInsert = "insert into pessoa(nome) values (?)";
 			if (this.insert(conn, sqlInsert)){
 				System.out.println("Inseriu corretamente...");
 			}
+		}
+		
+		if (executarInsertMilhoes){
+			this.insertMilhoesComPreparedStatement(conn);
 		}
 		conn.close();
 	}
@@ -63,6 +64,8 @@ public class Principal {
 			result = st.execute(sql);
 			st.close();
 		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return result;
@@ -82,6 +85,49 @@ public class Principal {
 
 		return retorno;
 	}
+	
+	
+	
+	/**
+	 * Avaliacao de desempenho com insertMilhoesComPreparedStatement
+	 * @param conn
+	 */
+	public void insertMilhoesComPreparedStatement(Connection conn){
+		try {
+			String sql = "insert into pessoa (nome) values (?)";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			//Statement st = conn.createStatement();
+			
+			// para acompanhar o andamento dos inserts, seto para auto commit
+			// igual a false, pois senao o commit seria feito apenas no final
+			conn.setAutoCommit(false);
+			
+			final int batchSize = 20000;
+			
+			for (int i = 0; i < 100000; i++){
+				ps.setString(1, "myName" + i);
+				ps.addBatch();
+				if (i % batchSize == 19999){
+					
+					long inicio = System.currentTimeMillis();
+					ps.executeBatch();
+					conn.commit();
+					long tempoGasto = (System.currentTimeMillis() - inicio) / (1000);
+					String tempo = Long.toString(tempoGasto);
+					
+					System.out.println("Inserido 20000 em " + tempo + " segundos...");
+				}
+			}
+			ps.executeBatch();
+			ps.close();
+			conn.commit();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	public Connection getConnection(){
 		try {
@@ -90,6 +136,11 @@ public class Principal {
 			String urlOracle = prop.getProperty("urlOracle");
 			String user = prop.getProperty("username");
 			String pass = prop.getProperty("password");
+			
+//			equivalente:
+//			InputStream is = this.getClass().getResourceAsStream("arq.properties");
+//			Properties p = new Properties();
+//			p.load(is);
 			
 			Connection c = null;
 			if (tipoBase.equals(TipoBase.ORACLE)){
@@ -128,3 +179,5 @@ public class Principal {
 		ORACLE;
 	}
 }
+
+
